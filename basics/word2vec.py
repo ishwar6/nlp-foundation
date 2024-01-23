74b603d0f714700fbda1ee915e209cde8bbeb5f8
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2024-01-24 01:12:24 +0530

    Update word2vec.py

--- basics/word2vec.py
+++ basics/word2vec.py
@@ -1,1 +1,5 @@
-# This is a technique to obtain vector representation of word. 
+# This is a technique to obtain vector representation of word. By a team lead by Tomáš Mikolov. 
+# As the name implies, word2vec represents each distinct word with a particular list of numbers called a VECTOR. 
+
+
+
