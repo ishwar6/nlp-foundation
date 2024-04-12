c6165530810cbd97e1e131c583f3f7bfdffe8bec
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2024-04-13 02:59:49 +0530

    Update lemmatization.py

--- basics/lemmatization.py
+++ basics/lemmatization.py
@@ -16,6 +16,7 @@ from nltk import pos_tag, word_tokenize
 
 nltk.download('averaged_perceptron_tagger')
 
+
 # It is based on the perceptron algorithm, a type of linear classifier used in supervised learning. The "averaged" part of the name refers to the way the algorithm computes its weights during training.
 # Instead of relying on the weights from the final iteration of training, it calculates the average weights across all iterations
 
